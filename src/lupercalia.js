e0596c3d66355ae5dc13b41197bf520ee6b05eeb
A. Quinton <argylewerewolf@gmail.com>
2017-01-25 17:47:41 -0800

    improved regex to capture St. & variations thereof

--- src/lupercalia.js
+++ src/lupercalia.js
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
     if (node.nodeType === 3) {
       var text = node.nodeValue;
-      var replacedText = text.replace(/(saint )?valentine'?s?/gi, 'Horny Werewolf');
+      var replacedText = text.replace(/(saint )?(st(\.)?)? ?valentine'?s?/gi, 'Horny Werewolf');
 
       if (replacedText !== text) {
       element.replaceChild(document.createTextNode(replacedText), node);
