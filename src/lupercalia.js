c4fd25c4cb702676799924f21c540e6728d85964
A. Quinton <argylewerewolf@gmail.com>
2017-01-25 18:11:50 -0800

    total number of replacements logged to console

--- src/lupercalia.js
+++ src/lupercalia.js
@@ -1,20 +1,23 @@
 const regex = /(saint )?(st(\.)?)? ?valentine'?s?/gi;
 const replacement = 'Horny Werewolf';
 const els = document.getElementsByTagName('*');
+let total = 0;
 
 for (var i = 0; i < els.length; i++) {
   const el = els[i];
-
   for (var j = 0; j < el.childNodes.length; j++) {
     const node = el.childNodes[j];
-
     if (node.nodeType === 3) {
-      const text = node.nodeValue;
       const replacedText = node.nodeValue.replace(regex, replacement);
-
-      if (replacedText !== text) {
+      if (replacedText !== node.nodeValue) {
         el.replaceChild(document.createTextNode(replacedText), node);
+        total++;
       }
     }
   }
+}
+
+if (total > 0) {
+  const message = total + ' rowdy werewolves on this page!';
+  (total < 10) ? console.info(message) : console.warn(message + ' Be careful!');
 }
